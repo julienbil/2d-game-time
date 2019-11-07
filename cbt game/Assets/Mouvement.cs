58cd123e3e35224ec834733d86dc39c0875f2d94
julienbil <julienbil38@live.fr>
2019-11-07 13:21:53 -0500

    Added movement xD
    ha ha fonni movment

---
+++ cbt game/Assets/Mouvement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouvement : MonoBehaviour
+{
+    GameObject player;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.gameObject;
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity += new Vector3(-1, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            rb.velocity += new Vector3(0, -1, 0);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.velocity += new Vector3(1, 0, 0);
+        }
+    }
+}
