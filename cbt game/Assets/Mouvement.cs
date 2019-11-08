1562972065c54e5e70d4318c649eac4d709fe6d6
Maxime <wixxwistiii@gmail.com>
2019-11-07 22:38:07 -0500

    moouv+retard+camera

--- cbt game/Assets/Mouvement.cs
+++ cbt game/Assets/Mouvement.cs
@@ -6,32 +6,42 @@ public class Mouvement : MonoBehaviour
 {
     GameObject player;
     Rigidbody rb;
+    public bool grounded;
+    public float maxVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         player = this.gameObject;
         rb = this.gameObject.GetComponent<Rigidbody>();
+        grounded = true;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.Space)&&grounded)
         {
-            rb.velocity += new Vector3(0, 1, 0);
+            rb.velocity += new Vector3(0, 10, 0);
+            grounded = false;
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)&& rb.velocity.x >= -maxVelocity)
         {
             rb.velocity += new Vector3(-1, 0, 0);
         }
-        if(Input.GetKey(KeyCode.S))
+        /*if(Input.GetKey(KeyCode.S))
         {
             rb.velocity += new Vector3(0, -1, 0);
-        }
-        if(Input.GetKey(KeyCode.D))
+        }*/
+        if(Input.GetKey(KeyCode.D)&& rb.velocity.x <= maxVelocity)
         {
             rb.velocity += new Vector3(1, 0, 0);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
--- cbt game/Assets/Retard.cs
+++ cbt game/Assets/Retard.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Retard : MonoBehaviour
 {
+    public float maxVelocity;
+    Rigidbody rb;
+    Transform pTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = transform.GetComponent<Rigidbody>();
+        pTransform = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < pTransform.position.x)
+            GoRight();
+        else if (transform.position.x > pTransform.position.x)
+            GoLeft();
+    }
+
+    void GoRight()
+    {
+        if(rb.velocity.x <= maxVelocity)
+            rb.velocity += new Vector3(1, 0, 0);
+    }
+
+    void GoLeft()
+    {
+        if (rb.velocity.x >= -maxVelocity)
+            rb.velocity += new Vector3(-1, 0, 0);
     }
 }
