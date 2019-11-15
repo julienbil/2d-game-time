e6399219bf5dfd1f517c968ed0c72c1c6bdce1cc
Maxime <wixxwistiii@gmail.com>
2019-11-14 22:03:07 -0500

    camera follow player but offset sucks whatever monkey u are dumb

---
+++ cbt game/Assets/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float offsetX, offsetY;
+    public Transform playerT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(playerT.position.x+offsetX, playerT.position.y+offsetY, transform.position.z);
+    }
+}
